c5cab1bfa3b05549cb5dc8d08d803d4550ba7293
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2025-06-18 19:07:56 -0300

    chore: files organization

--- src/app/modules/config/config.ts
+++
@@ -1,8 +0,0 @@
-const { PORT } = process.env;
-
-const getConfiguration = () => ({
-  port: parseInt(PORT, 10) || 3000,
-});
-
-export default getConfiguration;
-export type Configuration = ReturnType<typeof getConfiguration>;
