0eebfb43fab6d7bad193e6df9fbb98040a1204c5
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2025-06-18 19:11:19 -0300

    chore: files organization

--- dist/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { PORT } = process.env;
-const getConfiguration = () => ({
-    port: parseInt(PORT, 10) || 3000,
-});
-exports.default = getConfiguration;
-//# sourceMappingURL=config.js.map
