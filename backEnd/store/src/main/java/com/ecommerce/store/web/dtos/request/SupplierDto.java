08a00a55a6f49fc2c4e0300c65541962f7fa50b0
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2025-07-10 18:57:46 -0300

    feat: add exception handling for supplier not found and validation errors

--- backEnd/store/src/main/java/com/ecommerce/store/web/dtos/request/SupplierDto.java
+++ backEnd/store/src/main/java/com/ecommerce/store/web/dtos/request/SupplierDto.java
@@ -1,13 +1,22 @@
 package com.ecommerce.store.web.dtos.request;
 
 import com.ecommerce.store.entities.Address;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SupplierDto {
+
+    @NotBlank(message = "CNPJ é obrigatório")
     private String cnpj;
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+    @NotBlank(message = "Telefone é obrigatório")
     private String phone_number;
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+    @NotBlank(message = "Endereço é obrigatório")
     private Address address;
 }
