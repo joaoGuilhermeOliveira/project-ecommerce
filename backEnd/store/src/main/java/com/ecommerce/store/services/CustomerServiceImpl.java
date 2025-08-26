11d0b23d9cdb64d68f66a646703b52e0cfaa7369
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2025-08-26 19:25:21 -0300

    feat: integrate Keycloak user creation in CustomerService and update CustomerRequestDto

--- backEnd/store/src/main/java/com/ecommerce/store/services/CustomerServiceImpl.java
+++ backEnd/store/src/main/java/com/ecommerce/store/services/CustomerServiceImpl.java
@@ -15,6 +15,7 @@ import com.ecommerce.store.services.mapper.CustomerMapper;
 import com.ecommerce.store.web.dtos.responses.CustomerResponseDto;
 import com.ecommerce.store.web.dtos.requests.CustomerRequestDto;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,26 +24,32 @@ public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
-
+    private final KeycloakService keycloakService;
     @Autowired
-    public CustomerServiceImpl(CustomerRepository customerRepository, CustomerMapper customerMapper) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, CustomerMapper customerMapper,KeycloakService keycloakService) {
         this.customerRepository = customerRepository;
         this.customerMapper = customerMapper;
+        this.keycloakService = keycloakService;
     }
 
     @Override
     public void createCustomer(CustomerRequestDto customerRequestDto) {
-        log.info("Creating customer with CPF: {}", customerRequestDto.getCpf());
+        ResponseEntity<String> keycloakResponse = keycloakService.createUser(
+                customerRequestDto.getCpf(),
+                customerRequestDto.getName(),
+                customerRequestDto.getLastName(),
+                customerRequestDto.getPassword(),
+                customerRequestDto.getEmail()
+        );
 
-        if (customerRepository.existsByCpf(customerRequestDto.getCpf())) {
-            log.warn("Customer with CPF {} already exists.", customerRequestDto.getCpf());
-            throw new ConflictException("Customer with CPF " + customerRequestDto.getCpf() + " already exists.");
+        log.info("Creating customer with CPF: {}", customerRequestDto.getCpf());
+        if (keycloakResponse.getStatusCodeValue() == 201) {
+            customerRepository.save(customerMapper.toEntity(customerRequestDto));
+        } else {
+            throw new RuntimeException("Erro ao criar usuário no Keycloak: " + keycloakResponse.getBody());
         }
-        Customer customer = customerMapper.toEntity(customerRequestDto);
-        customer.setStatus(StatusEnum.ACTIVE);
-        customerRepository.save(customer);
-        log.info("Customer created successfully: {}", customer.getCpf());
     }
+
 
     @Override
     public CustomerResponseDto getCustomerByCpf(String cpf) {
--- backEnd/store/src/main/java/com/ecommerce/store/services/KeycloakService.java
+++ backEnd/store/src/main/java/com/ecommerce/store/services/KeycloakService.java
@@ -1,13 +1,20 @@
 package com.ecommerce.store.services;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ecommerce.store.keycloak.KeycloakProperties;
 import com.ecommerce.store.web.dtos.responses.KeycloakTokenResponseDto;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class KeycloakService {
@@ -36,5 +43,50 @@ public class KeycloakService {
                 .retrieve()
                 .bodyToMono(KeycloakTokenResponseDto.class)
                 .block();
+    }
+
+
+    private String getAdminAccessToken() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8081/realms/master/protocol/openid-connect/token";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", "admin-cli");
+        params.add("username", "admin");
+        params.add("password", "admin");
+        params.add("grant_type", "password");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
+
+        return (String) response.getBody().get("access_token");
+    }
+
+    public ResponseEntity<String> createUser(String username, String firstName, String lastName, String password, String email) {
+        String token = getAdminAccessToken();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8081/admin/realms/ecommerce/users";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", username);
+        user.put("firstName", firstName);
+        user.put("lastName", lastName);
+        user.put("email", email);
+        user.put("enabled", true);
+
+        Map<String, Object> credentials = new HashMap<>();
+        credentials.put("type", "password");
+        credentials.put("value", password);
+        credentials.put("temporary", false);
+        user.put("credentials", new Object[]{credentials});
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(user, headers);
+        return restTemplate.postForEntity(url, request, String.class, "ecommerce");
     }
 }
